c291153cc44b3c10ff45d1623b4a0e14dd37cbc2
Gabriel Deleon <notprivategabe@gmail.com>
2021-05-29 17:36:31 -0400

    started work on send_to_server conversion

--- patch_server/patch_server.go
+++ patch_server/patch_server.go
@@ -14,3 +14,46 @@
 //   You should have received a copy of the GNU General Public License
 //   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // ************************************************************************
+
+package patchserver
+
+import (
+	"errors"
+	"log"
+)
+
+type PatchServer interface {
+	Serve()
+}
+
+const (
+	MAX_PATCHES                    = 4096
+	PATCH_COMPILED_MAX_CONNECTIONS = 300
+	SERVER_VERSION                 = "0.0.1"
+	SEND_PACKET                    = 0x00
+	RECEIVE_PACKET_02              = 0x01
+	RECEIVE_PACKET_10              = 0x02
+	SEND_PACKET_0B                 = 0x03
+	MAX_SENDCHECK                  = 0x04
+	TCP_BUFFER_SIZE                = 65530
+	SOCKET_ERROR                   = -1
+)
+
+// "Encyption Data Struct"
+// Scaffolding for communication with PSO client...?
+type CryptSetup struct {
+	keys    [1024]uint32 //encryption stream
+	pc_posn uint32       //
+}
+
+var Crypt_PC_GetNextKey func(c *CryptSetup) uint32
+
+func send_to_server(sock int, packet []byte) error {
+	log.Print("Sending to patch server...")
+	//pktlen := len(packet)
+	// C code sends the message and compares size of response
+	// TODO: Suss out sending messages/socks in GO properly
+	err := errors.New("send_to_server(): failure")
+	log.Print(err)
+	return err
+}
